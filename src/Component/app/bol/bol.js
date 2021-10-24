9f0c538b035ffe444afa8a7699cabc661d98d440
Sasha Poghosyan <sash15xxx@mail.ru>
2021-10-24 19:28:17 +0400

    eli formatavorum

--- src/Component/app/bol/bol.js
+++ src/Component/app/bol/bol.js
@@ -1,47 +1,35 @@
 
-import React,{Component} from "react";
+import React, { Component } from "react";
 
 
-export default class Bol extends Component{
-   
-   
+export default class Bol extends Component {
 
-    render(){
-      
-
-       
+    render() {
         let text = this.props.text;
         let el = this.props.el;
         let i = this.props.i;
         let backgr = "one";
         let vichak = 'one_text';
-        
-         if(el.activ == true){
-            
-           backgr = "one_true";
-          }
-         
-         
-           
 
-      
- 
-        
-        return(
-            
+        if (el.activ == true) {
+            backgr = "one_true";
+        }
+
+        return (
+
             <>
 
-            
-                    
-               <div key={i}  className={backgr}   onClick={()=>{this.props.changActiv(i)}} ></div>
-                    
-       
-            
-           
-            
+
+
+                <div key={i} className={backgr} onClick={() => { this.props.changActiv(i) }} ></div>
+
+
+
+
+
             </>
         )
     }
 }
-    
+
 
--- src/Component/app/heder_rus/heder.js
+++ src/Component/app/heder_rus/heder.js
@@ -1,4 +1,4 @@
-import React,{Component} from "react";
+import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 import './heder.css';
 import { FaWifi } from "react-icons/fa";
@@ -8,117 +8,117 @@ import { FaCloud } from "react-icons/fa";
 
 
 
-export default class Heder_rus extends Component{
+export default class Heder_rus extends Component {
 
-    
-    render(){
+
+    render() {
 
         let month = null;
         let weekday = null;
 
-        switch(this.props.weekday){
-            case 0: weekday = 'Воскресенье';break;
-            case 1: weekday = 'Понедельник';break;
-            case 2: weekday = 'Вторник';break;
-            case 3: weekday = 'Среда';break;
-            case 4: weekday = 'Четверг';break;
-            case 5: weekday = 'Пятница';break;
-            case 6: weekday = 'Суббота';break;
-            
+        switch (this.props.weekday) {
+            case 0: weekday = 'Воскресенье'; break;
+            case 1: weekday = 'Понедельник'; break;
+            case 2: weekday = 'Вторник'; break;
+            case 3: weekday = 'Среда'; break;
+            case 4: weekday = 'Четверг'; break;
+            case 5: weekday = 'Пятница'; break;
+            case 6: weekday = 'Суббота'; break;
+
 
 
             default: weekday = 'շաբաթ';
         }
-        
 
-        switch(this.props.month){
-            case 0: month = "Январь";break;
-            case 1: month = 'Февраль';break;
-            case 2: month = 'Март';break;
-            case 3: month = 'Апрель';break;
-            case 4: month = 'Май';break;
-            case 5: month = 'Июнь';break;
-            case 6: month = 'Июль';break;
-            case 7: month = 'Август';break;
-            case 8: month = 'Сентябрь';break;
-            case 9: month = "Октябрь";break;
-            case 10: month = 'Ноябрь';break;
-            case 11: month = 'Декабрь';break;
-            
-            
+
+        switch (this.props.month) {
+            case 0: month = "Январь"; break;
+            case 1: month = 'Февраль'; break;
+            case 2: month = 'Март'; break;
+            case 3: month = 'Апрель'; break;
+            case 4: month = 'Май'; break;
+            case 5: month = 'Июнь'; break;
+            case 6: month = 'Июль'; break;
+            case 7: month = 'Август'; break;
+            case 8: month = 'Сентябрь'; break;
+            case 9: month = "Октябрь"; break;
+            case 10: month = 'Ноябрь'; break;
+            case 11: month = 'Декабрь'; break;
+
+
             default: month = "amis";
         }
-        return(
+        return (
             <>
-            <div className={'heder'}>
-               
+                <div className={'heder'}>
+
                     <div className={'logo'}><a class="logo" href="https://news.am/arm/"></a>
-                        
-                    
+
+
                     </div>
-                    
-                    
+
+
                     <div className={'lezu'}>
                         <ul>
-                            <div className={'hay'}><NavLink style={{textDecoration:'none'}} to={'/Arm'}>Հայ</NavLink></div>
-                            <div className={'eng'}> <NavLink style={{textDecoration:'none'}} to={'/Eng'}>Eng</NavLink> </div>
-                            <div style={{backgroundColor:'red'}} className={'rus'}><NavLink style={{textDecoration:'none',color:'white'}} to={'/Rus'}>Рус</NavLink></div>
+                            <div className={'hay'}><NavLink style={{ textDecoration: 'none' }} to={'/Arm'}>Հայ</NavLink></div>
+                            <div className={'eng'}> <NavLink style={{ textDecoration: 'none' }} to={'/Eng'}>Eng</NavLink> </div>
+                            <div style={{ backgroundColor: 'red' }} className={'rus'}><NavLink style={{ textDecoration: 'none', color: 'white' }} to={'/Rus'}>Рус</NavLink></div>
                         </ul>
-                        </div>
+                    </div>
 
-                        <div className ={'calendar'}>
+                    <div className={'calendar'}>
 
-                            <ul> 
+                        <ul>
                             <li>{weekday}</li><li>{month}</li><li className={'tiv'}>{this.props.day}</li>
-                            </ul>
+                        </ul>
 
+                    </div>
+
+
+                    <div className={'cheng'}>
+
+                        <div>USD<br /> 480.82</div>
+                        <div>EUR<br /> 564.15</div>
+                        <div>RUB<br /> 6.6</div>
+
+
+                    </div>
+
+
+                    <div className={'xarn'}>
+                        <div className={'exanak'}>Ереван C <FaCloud style={{ color: 'red' }} /> </div>
+                        <div className={'logos'}>
+                            <div className={'wifi'}><FaWifi /></div>
+                            <div className={'twiter'}><FaTwitter /></div>
+                            <div className={'f'}><FaFacebookF /></div>
+                            <div className={'me'}>me</div>
                         </div>
-                      
-
-                        <div className={'cheng'}>
-                           
-                            <div>USD<br/> 480.82</div>
-                            <div>EUR<br/> 564.15</div>
-                            <div>RUB<br/> 6.6</div>
-                        
-                                                       
-                        </div>
-
-
-                        <div className={'xarn'}>
-                            <div className={'exanak'}>Ереван C <FaCloud style={{color:'red'}}/> </div>
-                            <div className={'logos'}>
-                                <div className={'wifi'}><FaWifi/></div>
-                                <div className={'twiter'}><FaTwitter/></div>
-                                <div className={'f'}><FaFacebookF/></div>
-                                <div className={'me'}>me</div>
-                            </div>
-                        </div>
-                        <div className={'navbar'}>
-                            <div className={'news'}>News</div>
-                            <div className={'sport'}>Sport</div>
-                            <div className={'style'}>Style</div>
-                            <div className={'medicine'}>Medicine</div>
-                            <div className={'video'}>Video</div>
-                            <div className={'newsam'}>NEWS.am TV</div>
-                            <div className={'reviews'}>Reviews</div>
-                            <div className={'photo'}>Photo</div>
-                            <div className={'vakancies'}>Vakancies</div>
-                            <div className={'input'}>
-                            <input  />
+                    </div>
+                    <div className={'navbar'}>
+                        <div className={'news'}>News</div>
+                        <div className={'sport'}>Sport</div>
+                        <div className={'style'}>Style</div>
+                        <div className={'medicine'}>Medicine</div>
+                        <div className={'video'}>Video</div>
+                        <div className={'newsam'}>NEWS.am TV</div>
+                        <div className={'reviews'}>Reviews</div>
+                        <div className={'photo'}>Photo</div>
+                        <div className={'vakancies'}>Vakancies</div>
+                        <div className={'input'}>
+                            <input />
                             <div className={'search1'}></div>
 
-                            </div>
-                            
-                            
-                            
-                            
                         </div>
-                    
-                    
-                    
-                
-            </div>
+
+
+
+
+                    </div>
+
+
+
+
+                </div>
             </>
         )
     }
